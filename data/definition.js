43ebe957f1c3415e6fbfad97853e32f08ed2e6d0
ParadiceWien <verein@paradice.wien>
2025-06-22 16:05:13 +0200

    Update definition.js

--- data/definition.js
+++ data/definition.js
@@ -22,7 +22,7 @@ const fileQuestions = `questions-${language}.csv`;
 const intQuestions = 6;
 
 // Name of the CSV file with the board games in the /data directory
-const fileAnswers = `http://api.paradice.info/games-de.csv`;
+const fileAnswers = "http://api.paradice.info/games-de.csv";
 
 // File paths to system and CSS files
 // If you have several instances of this tool running, you can use absolute references to global files (therefore, these are not hardcoded)=
